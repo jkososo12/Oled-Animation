00df77166c6b95bf55ab1d347ea253325e4c3a1f
jkososo12 <kulijawaveteran@gmail.com>
2025-05-29 18:02:06 +0700

    modified source code

--- src/main.cpp
+++ src/main.cpp
@@ -3,33 +3,56 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include "../src/bitmap/happy.h"
+#include "../src/bitmap/sad.h"
+#include "../src/bitmap/confused.h"
 
 #define SCREEN_I2C_ADDR 0x3C 
 #define SCREEN_WIDTH 128    
 #define SCREEN_HEIGHT 64     
 #define OLED_RST_PIN -1      
 
-Adafruit_SSD1306 display(128, 64, &Wire, OLED_RST_PIN);
+#define BLUE_LED 2
+#define RED_LED 4
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST_PIN);
 
 void setup() {
   Serial.begin(9600);
 
+  pinMode(BLUE_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+
   if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_I2C_ADDR)) {
     Serial.println(F("SSD1306 allocation failed"));
-    for (;;);
+    for (;;); // Berhenti di sini jika gagal
   }
 
   display.clearDisplay();
-
-  display.display(); 
-  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_I2C_ADDR);
-  display.clearDisplay();
-  display.drawBitmap(0, 0, senyum, SCREEN_WIDTH, SCREEN_HEIGHT, WHITE);
   display.display();
-  delay(2000); // Display the image for 2 seconds
 }
-int frame = 0;
-void loop() {
-    
 
+void loop() {
+  // Contoh: atur nilai LED secara manual (untuk demo)
+  digitalWrite(BLUE_LED, LOW);  // Set LED biru ON
+  digitalWrite(RED_LED, HIGH);    // Set LED merah OFF
+
+  bool blueState = digitalRead(BLUE_LED);
+  bool redState = digitalRead(RED_LED);
+
+  display.clearDisplay(); // Selalu bersihkan display dulu
+
+  if (blueState && redState) {
+    display.drawBitmap(0, 0, confused, SCREEN_WIDTH, SCREEN_HEIGHT, WHITE);
+  } else if (redState) {
+    display.drawBitmap(0, 0, senyum, SCREEN_WIDTH, SCREEN_HEIGHT, WHITE);
+  } else if (!blueState && !redState) {
+    // Jangan tampilkan gambar apa-apa (kosongkan layar)
+    display.clearDisplay();
+  } else {
+    display.drawBitmap(0, 0, sad, SCREEN_WIDTH, SCREEN_HEIGHT, WHITE);
+  }
+
+  display.display();  
+  delay(2000); // Tampilkan gambar selama 2 detik
 }
+
